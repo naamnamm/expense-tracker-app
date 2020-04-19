5a8ca8d9d37c5e8a67940c1858042bcaaa84e4cc
Naam <naam.pt@gmail.com>
2020-04-19 12:59:59 -0400

    add total row and filter all feature

--- script.js
+++ script.js
@@ -45,17 +45,59 @@ function addExpense(e) {
     let descriptionCatagory = document.getElementById('catagory').value;
     let displayCatagory = document.getElementsByClassName('data-catagory')[0];
     displayCatagory.innerText = descriptionCatagory;
+    displayCatagory.classList = `data-catagory ${descriptionCatagory} all`;
 
     //grab dollar amount
     let amount = document.getElementById('amount').value;
     let displayAmount= document.getElementsByClassName('data-amount')[0];
-    displayAmount.innerText = '$' + amount;
+    displayAmount.innerText = `$ ${amount}`;
 
     //remove default row
     removeDefaultRow();
 
+    addTotalRow();
+
     clearField();
 }
+
+function addTotalRow() {
+    let table = document.getElementById('table-main').getElementsByTagName('tbody')[0];
+    let rows = document.getElementById('table-main').getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+    log(rows)
+    
+    log(Array.from(rows));
+    let rowArray = Array.from(rows)
+    if (rowArray.length >= 1) {
+        let lastRow = table.insertRow(-1);
+
+        let lastRowTd1 = lastRow.insertCell(0);
+        let lastRowTd2 = lastRow.insertCell(1);
+        
+        lastRowTd1.setAttribute('colspan', '4')
+        lastRowTd1.innerHTML = 'Total';
+        lastRowTd1.className = 'last-row';
+        lastRowTd2.className = 'last-row';
+    }
+
+    //calculate total
+    //if rowArray.length >= 1
+    
+    //grab lastRowTd2
+
+    //grab value of lastsibling of rowArray;
+
+    //reduce method to sum all value
+
+
+
+    //calculateTotal(rowArray);
+    
+}
+
+// function calculateTotal(rowArray) {
+//     if (rowArray )
+
+// }
 
 function getDate() {
     let date = new Date(document.getElementById('date').value);
@@ -82,6 +124,7 @@ function clearField() {
     document.getElementById('location').value = '';
     document.getElementById('description').value = '';
     document.getElementById('amount').value = '';
+    document.getElementById('catagory').selectedIndex = '0';
 }
 
 function removeDefaultRow() {
@@ -95,9 +138,9 @@ function removeDefaultRow() {
     }
 }
 
-log(document.getElementById('expense-content').firstElementChild);
-log(document.getElementById('expense-content').nextElementSibling);
-log(document.getElementsByClassName('first-expense-row'));
+// log(document.getElementById('expense-content').firstElementChild);
+// log(document.getElementById('expense-content').nextElementSibling);
+// log(document.getElementsByClassName('first-expense-row'));
 
 // function renderTable() {
 
@@ -113,6 +156,7 @@ function filterExpense (e) {
     //let rows = table.getElementsByTagName('tr');
 
     let filteredCatagory = e.target.value;
+    log(filteredCatagory);
 
     //forEach
     // Array.from(rows).forEach(function(row) {
@@ -126,17 +170,45 @@ function filterExpense (e) {
     // )
 
     //for loop
-    for (let row of table.rows) {
-        let catagory = row.lastElementChild.previousElementSibling.innerText;
-        if (catagory === filteredCatagory) {
+    // for (let row of table.rows) {
+    //     let catagory = row.lastElementChild.previousElementSibling.innerText;
+        
+    //     if (catagory === filteredCatagory) {
+    //         row.style.display = 'table-row';
+
+    //     } else {
+    //         row.style.display = 'none';
+    //     }
+                
+    // } 
+
+     //for loop
+     for (let row of table.rows) {
+        let catagoryCell = row.lastElementChild.previousElementSibling;
+        
+        if (catagoryCell.classList.contains(filteredCatagory)) {
             row.style.display = 'table-row';
+
         } else {
             row.style.display = 'none';
         }
                 
     } 
+    //add catarogy with class 'catagory all'
 
+    //if fourth child.classlist.contains(filtered)
+    //display = table row.
+
+    //else 
+    //display - none
+
+    //https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_filter_elements
+    
+    // if catagory === select // exit the loop >> display all rows
+    //Can I use break statement?
+    
 }
 
 
-    
+//if add new expense > display all 
+//total function > reduce method?
